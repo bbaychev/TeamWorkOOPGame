546d48049287b6d4d295c58aa29ca3daed0b953d
bbaychev <mb_int@abv.com>
2015-02-12 21:29:26 +0200

    Initial push

---
+++ Game/SplashScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game
+{
+    public class SplashScreen : GameScreen
+    {
+        public Image Image;
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            Image.LoadContent();
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+            Image.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            Image.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            Image.Draw(spriteBatch);
+        }
+    }
+}
