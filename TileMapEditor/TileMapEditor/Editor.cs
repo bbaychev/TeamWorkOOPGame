a012b3408cf952470e179cfa79c64507102de4ae
bbaychev <mb_int@abv.com>
2015-02-23 18:49:55 +0200

    Building the map editor

---
+++ TileMapEditor/TileMapEditor/Editor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TileMapEditor
+{
+    class Editor : GraphicsDeviceControl
+    {
+        private Map map;
+        private ContentManager content;
+        private SpriteBatch spriteBatch;
+
+        public Editor()
+        {
+            map = new Map();
+        }
+
+        protected override void Initialize()
+        {
+            content = new ContentManager(Services, "Content");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            XmlSerializer xml = new XmlSerializer(map.GetType());
+            Stream stream = File.Open("Load/Map.xml", FileMode.Open);
+            map = (Map)xml.Deserialize(stream);
+            map.Initialize(content);
+        }
+
+        protected override void Draw()
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            map.Draw(spriteBatch);
+            spriteBatch.End();
+        }
+    }
+}
