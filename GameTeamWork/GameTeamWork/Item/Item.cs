f79fae410aef8a31a23fdeebfe8893a29c1744fc
knaos <akaynarov@gmail.com>
2015-02-25 22:45:05 +0200

    Checking interfaces

--- GameTeamWork/GameTeamWork/Item/Item.cs
+++ GameTeamWork/GameTeamWork/Item/Item.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace GameTeamWork.Item
 {
    public  class Item : IItem
-    {
+    {   //TODO: Write more methods
         public int DamageBonus { get; set; }
         public int HealthBonus { get; set; }
 
