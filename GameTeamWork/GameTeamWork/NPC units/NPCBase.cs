0b0251d603f519af583a81880f63dae6b73623f5
knaos <akaynarov@gmail.com>
2015-02-26 15:57:53 +0200

    Add NPC class... NEEDS A LOT OF WORK!

---
+++ GameTeamWork/GameTeamWork/NPC units/NPCBase.cs
@@ -0,0 +1,32 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameTeamWork.NPC
+{
+    public class NPC_Base
+    {
+        private string ID;
+        
+        private bool isAttackable;
+        private int health = 1;
+
+        public NPC_Base(string id, bool isAttackable, int health)
+        {
+            this.ID = id;
+            
+            this.isAttackable = isAttackable;
+            this.health = health;
+        }
+
+
+    }
+}
---
+++ GameTeamWork/GameTeamWork/NPC units/NPCQuester.cs
@@ -0,0 +1,30 @@
+﻿
+﻿using GameTeamWork.NPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTeamWork.Npc
+{
+    public class NPC_Quester : NPC_Base
+    {   
+        private string questText;
+        
+           
+        public NPC_Quester(string id, bool isAttackable, int health, string questText)
+            :base(id,isAttackable, health)
+        {
+            this.questText = questText;
+        }
+
+        public virtual string GiveQuest()
+        {
+            //TODO: Implement quest giving logic
+                return this.questText;
+            
+
+            
+        }
+    }
+}
