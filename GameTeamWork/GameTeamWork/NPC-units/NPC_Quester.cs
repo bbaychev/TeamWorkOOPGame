e5f8549ec9d851f6642c519aaec849380424d919
knaos <akaynarov@gmail.com>
2015-02-26 15:44:44 +0200

    Added NPC bae class, and a deriving quester class..
    Still a lot of changes have to be made

---
+++ GameTeamWork/GameTeamWork/NPC-units/NPC_Quester.cs
@@ -0,0 +1,29 @@
+﻿using GameTeamWork.NPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTeamWork.NPC_units
+{
+    public class NPC_Quester : NPC_Base
+    {   
+        private string questText;
+        
+           
+        public NPC_Quester(string id, bool isAttackable, int health, string questText)
+            :base(id,isAttackable, health)
+        {
+            this.questText = questText;
+        }
+
+        public virtual string GiveQuest()
+        {
+            //TODO: Implement quest giving logic
+                return this.questText;
+            
+
+            
+        }
+    }
+}
