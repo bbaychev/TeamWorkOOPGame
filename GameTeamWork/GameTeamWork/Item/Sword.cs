e43bd4cbe1fc34b6ec56f8ff4c07127ce9d90be6
knaos <akaynarov@gmail.com>
2015-02-26 16:03:54 +0200

    Added weapon and potion

---
+++ GameTeamWork/GameTeamWork/Item/Sword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTeamWork.Item
+{
+    public class Sword : Item
+    {
+        public Sword()
+            :base(2,0)
+        {
+
+        }
+    }
+}
